b744f5d43bbbe324f2ea2ae407e2047c3e4462a2
idhayaprasanth <idhayaprasanth56@gmail.com>
2025-12-14 19:02:21 +0530

    add.py file created

---
+++ add.py
@@ -0,0 +1,15 @@
+class Add:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def compute(self):
+        return self.a + self.b
+    
+    def display(self):
+        print(f"Adding {self.a} and {self.b}")
+        result = self.compute()
+        print(f"Result: {result}")
+
+if __name__ == "__main__":
+    adder = Add(5, 7)
+    adder.display()
